faab0649f9a76b0adc18bee9ddb7caa88276ce54
Dmytro Dordovskyi <dordovskydmitry@gmail.com>
2017-11-06 23:35:47 +0100

    Implement react

--- go/react/react.go
+++ go/react/react.go
@@ -4,7 +4,7 @@ const testVersion = 5
 
 type ConcreteInputCell struct {
 	value int
-	r *ReactSystem
+	r     *ReactSystem
 }
 
 func (in *ConcreteInputCell) Value() int {
@@ -14,46 +14,61 @@ func (in *ConcreteInputCell) Value() int {
 func (in *ConcreteInputCell) SetValue(value int) {
 	if in.value != value {
 		in.value = value
-		updateCells(in.r.dependants[in])
+		markCells(in.r.dependants[in])
+		updateCellsFromRoot(in)
 	}
 }
 
 type ConcreteComputeCell struct {
-	value int
+	value       int
 	constructor (func() int)
-	r *ReactSystem
-	callbacks [](*func(int))
+	r           *ReactSystem
+	callbacks   [](*func(int))
 }
 
 func (c *ConcreteComputeCell) Value() int {
 	return c.value
 }
 
-func updateComputeCellValues(c *ConcreteComputeCell) {
-	oldValue := c.value
-	c.value = c.constructor()
+func markCells(cells []*ConcreteComputeCell) {
+	for _, c := range cells {
+		c.r.marked[c] += 1
+		markCells(c.r.dependants[c])
+	}
+}
 
-	if c.value != oldValue {
-		updateCells(c.r.dependants[c])
-		for _, callback := range c.callbacks {
-			(*callback)(c.value)
+func updateCellsFromRoot(in *ConcreteInputCell) {
+	updateQueue := in.r.dependants[in]
+	for len(updateQueue) > 0 {
+		elem := updateQueue[0]
+		if in.r.marked[elem] == 0 {
+			updateQueue = updateQueue[1:]
+			continue
 		}
+		if in.r.marked[elem] == 1 {
+			updateValue(elem)
+		}
+		in.r.marked[elem] -= 1
+		updateQueue = append(updateQueue[1:], elem.r.dependants[elem]...)
 	}
 }
 
-func updateCells(cells []*ConcreteComputeCell) {
-	for _, cell := range cells {
-		updateComputeCellValues(cell)
+func updateValue(c *ConcreteComputeCell) {
+	newValue := c.constructor()
+	if newValue != c.value {
+		for _, callback := range c.callbacks {
+			(*callback)(newValue)
+		}
+		c.value = newValue
 	}
 }
-
 
 type ConcreteCanceler struct {
 	f (*func(int))
 }
 
 func (canceler ConcreteCanceler) Cancel() {
-	*canceler.f = func(int){}
+	*canceler.f = func(int) {}
 }
 
 func (c *ConcreteComputeCell) AddCallback(f func(int)) Canceler {
@@ -63,13 +78,14 @@ func (c *ConcreteComputeCell) AddCallback(f func(int)) Canceler {
 
 type ReactSystem struct {
 	dependants map[Cell]([]*ConcreteComputeCell)
+	marked     map[*ConcreteComputeCell]int
 }
 
 func (r *ReactSystem) CreateInput(v int) InputCell {
 	return &ConcreteInputCell{value: v, r: r}
 }
 
-func (r *ReactSystem) CreateCompute1(in Cell, f (func(int) int)) ComputeCell {
+func (r *ReactSystem) CreateCompute1(in Cell, f func(int) int) ComputeCell {
 	c := createCompute(r, func() int { return f(in.Value()) })
 	r.dependants[in] = append(r.dependants[in], &c)
 	return &c
@@ -83,9 +99,10 @@ func (r *ReactSystem) CreateCompute2(in1 Cell, in2 Cell, f func(int, int) int) C
 }
 
 func createCompute(r *ReactSystem, f func() int) ConcreteComputeCell {
-	return ConcreteComputeCell{constructor: f, value: f(), r: r}
+	val := f()
+	return ConcreteComputeCell{constructor: f, value: val, r: r}
 }
 
 func New() Reactor {
-	return &ReactSystem{dependants: make(map[Cell]([](*ConcreteComputeCell)), 0)}
+	return &ReactSystem{dependants: make(map[Cell]([](*ConcreteComputeCell)), 0), marked: make(map[*ConcreteComputeCell]int, 0)}
 }
