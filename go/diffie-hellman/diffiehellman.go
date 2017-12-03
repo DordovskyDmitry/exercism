e0651e52e4589dbbef004d54ba28b7295a4d9f85
Dmytro Dordovskyi <dordovskydmitry@gmail.com>
2017-12-03 17:32:20 +0100

    Cleanup DiffieHellman

--- go/diffie-hellman/diffiehellman.go
+++ go/diffie-hellman/diffiehellman.go
@@ -5,25 +5,10 @@ import (
 	"math/big"
 )
 
-var one = big.NewInt(1)
-var two = big.NewInt(2)
-
 func PrivateKey(p *big.Int) *big.Int {
-	if p.Cmp(one) == 0 {
-		return p
-	}
-
-	nBig, err := rand.Int(rand.Reader, new(big.Int).Sub(p, two))
-
-	if err != nil {
-		panic(err)
-	}
-
-	if nBig.Cmp(two) < 0 {
-		nBig.Add(nBig, two)
-	}
-
-	return nBig
+	two := big.NewInt(2)
+	nBig, _ := rand.Int(rand.Reader, new(big.Int).Sub(p, two))
+	return nBig.Add(nBig, two)
 }
 
 func PublicKey(private, p *big.Int, g int64) *big.Int {
